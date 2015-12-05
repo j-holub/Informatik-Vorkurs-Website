10d226bb0507fe39c323337d8526cdb55bb15c58
Jan Holub <Jan-Holub@gmx.net>
2015-12-06 00:00:29 +0100

    Turniere (Tournaments) implantiert. User können maximal eine Roboter pro Turnier anlegen und auch wieder austragen.

--- client/Tournament/tournament.js
+++ client/Tournament/tournament.js
@@ -5,6 +5,27 @@ Template.tournaments.helpers({
 	}
 });
 
+
+
+Template.tournamentDetail.helpers({
+	userHasRobotInTournament: function () {
+		// Roboter des Users
+		var userRobots = Robots.find({belongsTo: Meteor.userId()}).fetch();
+		// Roboter im Turnier
+		var robotsInTournament = Tournaments.findOne({_id: this._id}).participants;
+		var userHasRobotInTournament = false;
+		// Checke für jeden Roboter ob er im Turnier ist
+		userRobots.forEach(function (robot) {
+			if($.inArray(robot._id, robotsInTournament) > -1){
+				userHasRobotInTournament = true;
+			}
+		});
+		return userHasRobotInTournament;
+	}
+});
+
+
+
 Template.createTournament.events({
 	'submit form': function (event) {
 		// Verhindert, dass das Formular abgeschickt, und die Seite neu geladen wird
@@ -18,5 +39,59 @@ Template.createTournament.events({
 		});
 		// Input Felder leeren
 		$('[name=tournamentName]').val('');
+	}
+});
+
+
+
+Template.tournamentParticipants.helpers({
+	// TODO: fehler werfen wenn roboter schon im Turnier ist oder der User bereits ein Roboter im Turnier hat
+	particpatingRobots: function () {
+		var participantIds = Tournaments.findOne({_id: this._id}).participants;
+		return Robots.find({_id: {$in: participantIds}}, {sort: {name: 1}});
+	}
+});
+
+
+
+Template.tournamentEntry.helpers({
+	listUserRobots: function () {
+		var currentUserId = Meteor.userId();
+		return Robots.find({belongsTo: currentUserId}, {sort: {name: 1}});
+	}
+});
+
+Template.tournamentEntry.events({
+	'submit form': function (event) {
+		// Verhindert, dass das Formular abgeschickt, und die Seite neu geladen wird
+		event.preventDefault();
+		// Die RoboterId raussuchen
+		var chosenRobotId = $('[name=availableRobots]').val();
+		// In das richtige Turnier eintragen
+		Tournaments.update({_id: this._id}, {$push: {participants: chosenRobotId}});
+	}
+});
+
+
+
+Template.tournamentRobot.helpers({
+	// sucht zu einer UserId ddes Uploaders en Vor- und Nachnamen raus
+	uploaderName: function (uploaderId) {
+		var uploader = Meteor.users.findOne({_id: uploaderId});
+		var name = uploader.profile.firstname + " " + uploader.profile.lastname;
+		return name;
+	},
+	belongsToUser: function(){
+		// this._id referenziert die ID des Objeckts mit dem das Template gerendert wurde
+		return Meteor.userId() == this.belongsTo;
+	}
+});
+
+Template.tournamentRobot.events({
+	'click [name=removeRobot]': function () {
+		// TODO: hier eine bessere Methode suchen um an das richtige Turnier zu kommen
+		var tournamentId = $('h3').data('id');
+		// this._id referenziert den Roboter des Templates
+		Tournaments.update({_id: tournamentId}, {$pull: {participants: this._id}});
 	}
 });

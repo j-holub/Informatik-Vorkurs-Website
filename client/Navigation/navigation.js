397d6307fd536e1b629c1cc223344084598a63c8
Jan Holub <Jan-Holub@gmx.net>
2015-12-16 21:05:08 +0100

    Navigationsbug gefixt, bei der die Avatar über dem Menü angezeigt wurde. Desktop Navigation ganz nach links verlegt.

--- client/Navigation/navigation.js
+++ client/Navigation/navigation.js
@@ -3,5 +3,6 @@ Template.navigation.events({
 	'click #menuButton': function (event) {
 		event.preventDefault();
 		$('#navi').toggleClass('collapsed');
+		$('#menuButton').toggleClass('active');
 	}
 });

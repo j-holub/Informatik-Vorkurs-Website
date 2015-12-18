155a10797b6210dbe70edf913d9b27ead2e8b165
Jan Holub <Jan-Holub@gmx.net>
2015-12-18 23:00:27 +0100

    Die Mobile Navigation schließt jetzt bei anklicken eines Menüpunktes

--- client/Navigation/navigation.js
+++ client/Navigation/navigation.js
@@ -1,8 +1,16 @@
 
 Template.navigation.events({
+	// Öffnet das Menü
 	'click #menuButton': function (event) {
 		event.preventDefault();
 		$('#navi').toggleClass('collapsed');
 		$('#menuButton').toggleClass('active');
+	},
+	// Schließt das Menü bei drücken eines Links
+	'click #navi a': function(event){
+		if($('#menuButton').hasClass('active') && !$('#navi').hasClass('collapsed')){
+			$('#menuButton').removeClass('active');
+			$('#navi').addClass('collapsed');
+		}
 	}
 });

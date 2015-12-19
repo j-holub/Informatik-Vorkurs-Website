f4fe2521ad4f5f1950a42d2813fb134c039c70df
Jan Holub <Jan-Holub@gmx.net>
2015-12-19 02:21:21 +0100

    Landing Page vorläufig fertig gestellt. Ist allerdings noch nicht so responsive

---
+++ client/Misc/landingPage.js
@@ -0,0 +1,26 @@
+Template.landingPage.onRendered(function(){
+	// viewport Höhe holen
+	var viewportHeight = $(window).height();
+	// landingpage Bild auf diese Höhe setzen
+	$('#landingPageImage').css('height', viewportHeight);
+
+	// Pfeil setzten
+	$('#downArrow').css('top', 0.85 * viewportHeight);
+
+	// größe der bereiche setzen
+	$('section').css('min-height', 0.8 * viewportHeight);
+});
+
+Template.landingPage.events({
+	'click #downArrow i': function (event) {
+		$(window).scrollTo('#information', 800);
+	},
+	'click #loginButton': function(event) {
+		$(window).scrollTo('#loginRegisterArea', 800);
+		$('.login [name=email]').focus();
+	},
+	'click #registerButton': function(event) {
+		$(window).scrollTo('#loginRegisterArea', 800);
+		$('.register [name=email]').focus();
+	}
+});

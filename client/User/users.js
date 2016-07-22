0905fa9b78ce06e91eb2fd9d775cbe8c114b20c8
Jan Holub <j_holub11@cs.uni-kl.de>
2016-07-22 22:00:40 +0200

    Neues Paket zum erkennen ob Desktop oder Mobile hinzugefügt. Dadurch wird die Usersuche nur auf dem Desktop direkt fokusiert

--- client/User/users.js
+++ client/User/users.js
@@ -71,8 +71,11 @@ Template.users.events({
 Template.users.rendered = function () {
 	// führt beim rendern des Templates direkt eine leer Suche aus um alle User anzuzeigen
 	UserSearch.search(' ');
-	// fokusiert das Suchfeld (außer im Firefox)
-	$('[name=userSearch]').focus();
+	
+	if(Meteor.Device.isDesktop()){
+		// fokusiert das Suchfeld (außer im Firefox)
+		$('[name=userSearch]').focus();
+	}
 };
 
 

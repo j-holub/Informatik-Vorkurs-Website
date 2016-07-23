8e2987d1b9c1d3ecdc56ba4b2b5acfedbac9bf17
Jan Holub <j_holub11@cs.uni-kl.de>
2016-07-24 01:10:11 +0200

    Den Streifen mit den 3 Elementen optisch verbessert

--- client/Misc/landingPage.js
+++ client/Misc/landingPage.js
@@ -3,13 +3,22 @@ Template.landingPage.onRendered(function(){
 	var viewportHeight = $(window).height();
 	// landingpage Bild auf diese Höhe setzen
 	$('#landingPageImage').css('height', viewportHeight);
-	$('.verticalCenter').css('height', viewportHeight);
+	$('#landingPageImage .verticalCenter').css('height', viewportHeight);
 
 	// Pfeil setzten
 	$('#downArrow').css('top', 0.85 * viewportHeight);
 
-	// größe der bereiche setzen
-	$('section').css('min-height', 0.8 * viewportHeight);
+	
+
+	if(Meteor.Device.isDesktop()){
+		// größe der bereiche setzen
+		$('section').css('min-height', 0.8 * viewportHeight);
+
+		// stripe bereich gesondert anpassen
+		$('#stripe').css('min-height', 0.4 * viewportHeight);
+		$('#stripe .verticalCenter').css('height', 0.4 * viewportHeight);
+			
+	}
 });
 
 Template.landingPage.events({

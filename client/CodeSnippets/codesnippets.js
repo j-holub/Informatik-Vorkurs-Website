2ad1baccf65e941c595ec8e11fd0942476830eb0
Jan Holub <j_holub11@cs.uni-kl.de>
2016-07-27 14:40:39 +0200

    CodeSnippets Design verbessert

--- client/CodeSnippets/codesnippets.js
+++ client/CodeSnippets/codesnippets.js
@@ -1,7 +1,48 @@
 Template.codesnippets.helpers({
 	listSnippets: function () {
 		return CodeSnippets.find({}, {sort: {'name': 1}});
+	},
+	linesOfCode: function() {
+		var number = (this.snippet.match(/\n/g) || []).length + 1;
+		switch(number){
+			case 1: 
+				return "<i class=\"mdi mdi-numeric-1-box-outline\"></i>";
+				break;
+			case 2: 
+				return "<i class=\"mdi mdi-numeric-2-box-outline\"></i>";
+				break;
+			case 3: 
+				return "<i class=\"mdi mdi-numeric-3-box-outline\"></i>";
+				break;
+			case 4: 
+				return "<i class=\"mdi mdi-numeric-4-box-outline\"></i>";
+				break;
+			case 5: 
+				return "<i class=\"mdi mdi-numeric-5-box-outline\"></i>";
+				break;
+			case 6: 
+				return "<i class=\"mdi mdi-numeric-6-box-outline\"></i>";
+				break;
+			case 7: 
+				return "<i class=\"mdi mdi-numeric-7-box-outline\"></i>";
+				break;
+			case 8: 
+				return "<i class=\"mdi mdi-numeric-8-box-outline\"></i>";
+				break;
+			case 9: 
+				return "<i class=\"mdi mdi-numeric-9-box-outline\"></i>";
+				break;
+			default:
+				return "<i class=\"mdi mdi-numeric-9-plus-box-outline\"></i>";
+				break;
+		}
 	}
+});
+
+Template.codesnippets.onRendered(function(){
+	$('h1').fitText(1.5, {
+		maxFontSize: '50em'
+	});
 });
 
 Template.snippet.onRendered(function(){
@@ -34,7 +75,9 @@ Template.snippet.helpers({
 Template.addSnippetModal.events({
 	'click #openSnippetModalButton': function(){
 		$('#snippetModal').css('display', 'block');
-		$('[name="snippetName"]').focus();
+		if(Meteor.Device.isDesktop()){
+			$('[name="snippetName"]').focus();
+		}
 	},
 	'click .modalBackground': function (event){
 		if(!(event.target != $('.modalBackground')[0])){

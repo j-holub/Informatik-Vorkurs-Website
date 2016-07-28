5c3bdd0f03c3f97e48f8de09f1ac31cb4682d6e6
Jan Holub <j_holub11@cs.uni-kl.de>
2016-07-28 16:46:53 +0200

    Mobile Navigation neu designt. Sieht jetzt geil aus

--- client/Navigation/navigation.js
+++ client/Navigation/navigation.js
@@ -1,16 +1,44 @@
-
 Template.navigation.events({
 	// Öffnet das Menü
 	'click #menuButton': function (event) {
 		event.preventDefault();
-		$('#navi').toggleClass('collapsed');
+		$('#naviWrapper').toggleClass('active');
 		$('#menuButton').toggleClass('active');
+		// backgroundblur
+		$('#backgroundDarken').toggleClass('active');
+		// $('#naviWrapper').toggleClass()
+		if($('body').css('overflow') != 'hidden'){
+			$('body').css('overflow', 'hidden');	
+		}
+		else{
+			$('body').css('overflow', 'auto');		
+		}
+		
 	},
 	// Schließt das Menü bei drücken eines Links
-	'click #navi a': function(event){
-		if($('#menuButton').hasClass('active') && !$('#navi').hasClass('collapsed')){
-			$('#menuButton').removeClass('active');
-			$('#navi').addClass('collapsed');
+	'click #navi li a': function(event){
+		if($('#naviWrapper').hasClass('active')){
+			$('#naviWrapper').removeClass('active');
 		}
+		if($('#menuButton').hasClass('active')){
+			$('#menuButton').removeClass('active');
+		}
+		if($('#backgroundDarken').hasClass('active')){
+			$('#backgroundDarken').removeClass('active');
+		}
+		$('body').css('overflow', 'auto');
+	},
+	// Schließt das Menü bei drücken des ausgergrauten Hintergrunds
+	'click #backgroundDarken': function(event){
+		if($('#naviWrapper').hasClass('active')){
+			$('#naviWrapper').removeClass('active');
+		}
+		if($('#menuButton').hasClass('active')){
+			$('#menuButton').removeClass('active');
+		}
+		if($('#backgroundDarken').hasClass('active')){
+			$('#backgroundDarken').removeClass('active');
+		}
+		$('body').css('overflow', 'auto');
 	}
 });

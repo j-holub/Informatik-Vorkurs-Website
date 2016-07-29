f3bf0e8a2aa9d38950cf22802f5c9c603d51d339
Jan Holub <j_holub11@cs.uni-kl.de>
2016-07-29 23:40:22 +0200

    Das X Icon wird jetzt im Profil immer richtig angezeigt; #fixes #2

--- client/User/userProfile.js
+++ client/User/userProfile.js
@@ -4,7 +4,38 @@ Template.profile.helpers({
 	},
 	hasRobots: function(id){
 		return Robots.find({'belongsTo': id}).count() > 0;
-	}
+	},
+	// das ganze MUSS als reaktiver Helper gemacht werden, da onRendered nicht aufgerufen wird,
+	// wenn man von einem anderen Profil auf sein eigenes Wechselt. Da dort das Template nicht 
+	// neu gerendert, sondern nur die Daten verändert werden
+	// Das ganze gibt es halt dann leider doppelt, aber muss erstmal so
+	addDeleteProfilePicButton: function(){
+
+		// FitTextJs
+		$('h2').fitText(1.2);
+
+		// ID des aktiven Nutzer
+		// durch diesen Aufruf wird der Helper reaktiv
+		var currentUser = Meteor.user();
+		// ID des Users des Profils
+		var userId = Template.currentData()._id;
+		if(userId == currentUser._id){
+			// Hoverklasse hinzufügen
+			// console.log($('#userProfile .avatar i'));
+			$('#userProfile .avatar').addClass('avatar-hover');
+			if(Meteor.user().profile.profilePicData != null){
+				var avatarBlock = $('#userProfile .avatar');
+				var icon = "<i class=\"fa fa-times fa-3x\" id=\"deleteProfilePic\"></i>";
+				avatarBlock.append(icon);
+			}
+			else{
+				$('#deleteProfilePic').remove();
+			}
+		}
+		else{
+			$('#userProfile .avatar').removeClass('avatar-hover');
+		}
+	},
 });
 
 

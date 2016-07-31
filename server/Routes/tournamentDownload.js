ee4a6f41da0e1570b0f2061eef3fd6a155f9811b
Jan Holub <j_holub11@cs.uni-kl.de>
2016-07-31 15:52:51 +0200

    Das Zippen beim Turnierdownload funktioniert jetzt komplett im RAM. Es werden keien Dateien mehr angelegt oder ähnliches

--- server/Routes/tournamentDownload.js
+++ server/Routes/tournamentDownload.js
@@ -8,75 +8,34 @@ Router.route('/tournaments/:_id/download', {
 	template: 'downloadRobots',
 	where: 'server',
 	action: function(){
+
 		// Das turnier holen
 		var tournament = Tournaments.findOne({_id: this.params._id});
 
-		// sollte der Ordner für die zips aus irgend einem Grund nicht existieren
-		// einfach erstellen
-		if(!fs.existsSync(`${process.env.PWD}/public/zips`)){
-			fs.mkdirSync(`${process.env.PWD}/public/zips`);
-		}
-
-		// das zip archiv
+		// Ziparchiv erstellen
 		var archive = new AdmZip();
-		var archiveName = `${tournament.name}.zip`
 
-		// liste der Dateien um sie danach wieder zu löschen
-		var fileList = new Array(tournament.participants.length);
-
-		// jeder Roboter muss in sein ursprünglichen namen umbenannt und in das 
-		// Zip archiv gepackt werden
+		// Jeden Roboter in das Ziparchiv packen
 		tournament.participants.forEach(function (participantId) {
 			var robot = Robots.findOne({_id: participantId});
 			var data  = RobotData.findOne({_id: robot.data});
-			
-			var filePath = `${process.env.PWD}/.meteor/local/cfs/files/robotData/${data.copies.robotData.key}`;
 
+			var owner = Meteor.users.findOne({_id: robot.belongsTo});
 
-			var newFilePath = "";
-			// überprüfuen ob ein Roboter mit dem selben Namen bereits vorhande ist
-			if(fs.existsSync(`${process.env.PWD}/public/zips/${robot.name}.jar`)){
-				// Uploader name heraussuchen
-				var uploader = Meteor.users.findOne(robot.belongsTo);
-				newFilePath = `${process.env.PWD}/public/zips/${robot.name}_${uploader.profile.firstname}-${uploader.profile.lastname}.jar`
-			}
-			else{
-				newFilePath = `${process.env.PWD}/public/zips/${robot.name}.jar`;
-			}
-
-			// Datei in den public Ordner kopieren und umbennen
-			fs.linkSync(filePath, newFilePath);
-			archive.addLocalFile(newFilePath);
-
-			// datei in der Liste speichern um sie wieder zu löschen
-			fileList.push(newFilePath);
-
+			archive.addFile(`${owner.profile.firstname} ${owner.profile.lastname} - ${robot.name}.jar`, new Buffer(data.url()));
 		});
 
-		// das zip archiv auf die Platte schreiben
-		archive.writeZip(`${process.env.PWD}/public/zips/${archiveName}`);
+		// Ein Buffer des Archivs erzeugen
+		var zipBuffer = archive.toBuffer();
 
-		// alle kopierten Dateien löschen
-		fileList.forEach(function (robot) {
-			fs.unlinkSync(robot);
-		});
-
-		// die Datei der HTTP response anhängen
-		var zipData = fs.readFileSync(`${process.env.PWD}/public/zips/${archiveName}`);
-
+		// Header setzen
 		var headers = {
-			'Content-type': 'application/octet-stream',
-			'Content-Disposition': 'attachment; filename=' + `${archiveName}`
+			'Content-type': 'application/zip',
+			'Content-Disposition': 'attachment; filename=' + `${tournament.name}.zip`
 		}
 
 		this.response.writeHead(200, headers);
-		this.response.end(zipData);
-	},
-	// löscht die zip am Ende wieder
-	onAfterAction: function(){
-		var tournament = Tournaments.findOne({_id: this.params._id});
-		if(fs.existsSync(`${process.env.PWD}/public/zips/${tournament.name}.zip`)){
-			fs.unlinkSync(`${process.env.PWD}/public/zips/${tournament.name}.zip`);
-		}
+		// das archiv mitschicken
+		this.response.end(zipBuffer);
 	}
 });

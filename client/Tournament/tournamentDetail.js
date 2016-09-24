e4cf347992fb7fb4d78421858d8e19752f283d70
Jan Holub <j_holub11@cs.uni-kl.de>
2016-09-24 17:23:05 +0200

    Man kann sich per Gruppe in einem Turnier eintragen und auch wieder austragen

--- client/Tournament/tournamentDetail.js
+++ client/Tournament/tournamentDetail.js
@@ -1,13 +1,13 @@
 Template.tournamentParticipants.helpers({
-	particpatingRobots: function () {
+	particpatingGroups: function () {
 		var participantIds = Tournaments.findOne({_id: this._id}).participants;
-		var robots = Robots.find({_id: {$in: participantIds}}, {sort: {name: 1}}).fetch();
+		var groups = Groups.find({_id: {$in: participantIds}}, {sort: {name: 1}}).fetch();
 
 		var formattedResultList;
-		if(robots.length > 3){
+		if(groups.length > 3){
 
 			// Rückgabeliste basteln
-			formattedResultList = new Array(Math.ceil(robots.length / 3));
+			formattedResultList = new Array(Math.ceil(groups.length / 3));
 			
 			// neues Array in jedem eintrag erstellen
 			for (var i  = 0; i  < formattedResultList.length; i++) {
@@ -15,36 +15,34 @@ Template.tournamentParticipants.helpers({
 			};
 
 			// Die Suchergebnisse entsprechend einsortieren
-			for (var i = 0; i < robots.length; i++) {
-				formattedResultList[Math.floor(i/3)][i % 3] = robots[i];
+			for (var i = 0; i < groups.length; i++) {
+				formattedResultList[Math.floor(i/3)][i % 3] = groups[i];
 			};
 
 		}
 		else{
 			formattedResultList = new Array(1);
-			formattedResultList[0] = robots;
+			formattedResultList[0] = groups;
 		}
 
 		// an den Client liefern
 		return formattedResultList;
-
 	}
 });
 
 Template.tournamentDetail.helpers({
-	userHasRobotInTournament: function () {
-		// Roboter des Users
-		var userRobots = Robots.find({belongsTo: Meteor.userId()}).fetch();
-		// Roboter im Turnier
-		var robotsInTournament = Tournaments.findOne({_id: this._id}).participants;
-		var userHasRobotInTournament = false;
-		// Checke für jeden Roboter ob er im Turnier ist
-		userRobots.forEach(function (robot) {
-			if($.inArray(robot._id, robotsInTournament) > -1){
-				userHasRobotInTournament = true;
-			}
-		});
-		return userHasRobotInTournament;
+	userGroupIsInTournament: function () {
+		// Gruppe des Users
+		var usergroup = Groups.findOne({members: Meteor.userId()});
+		// Teilnehmerlist
+		var tournamentParticipants = Tournaments.findOne(this._id).participants;
+
+		if(usergroup){
+			return ($.inArray(usergroup._id, tournamentParticipants) == 0);
+		}
+		else{
+			return false;
+		}
 	},
 	// Erzeugt einen schöner formatierten Datum String aus dem Date Object des Turniers
 	displayDate: function () {
@@ -87,10 +85,6 @@ Template.tournamentDetail.onRendered(function(){
 
 
 Template.tournamentEntry.helpers({
-	listUserRobots: function () {
-		var currentUserId = Meteor.userId();
-		return Robots.find({belongsTo: currentUserId}, {sort: {name: 1}});
-	},
 	userHasGroup: function () {		
 		return Groups.find({members: Meteor.userId()}).count() > 0;
 
@@ -125,34 +119,30 @@ Template.tournamentEntry.events({
 });
 
 
-Template.tournamentRobot.helpers({
-	// sucht zu einer UserId ddes Uploaders en Vor- und Nachnamen raus
-	uploaderName: function (uploaderId) {
-		var uploader = Meteor.users.findOne({_id: uploaderId});
-		if(uploader){
-			var name = uploader.profile.firstname + " " + uploader.profile.lastname;
-			return name;
-		}
-	},
-	belongsToUser: function(){
-		// this._id referenziert die ID des Objeckts mit dem das Template gerendert wurde
-		return Meteor.userId() == this.belongsTo;
+Template.tournamentGroup.helpers({
+	userIsMember: function (){
+		// this._id referenziert die ID des Objeckts (Group) mit dem das Template gerendert wurde
+		return $.inArray(Meteor.userId(), this.members) == 0; 
 	},
 	// schaut ob das Turnier schon beendet wurde
-	ended: function() {
-		// TODO: ier eine sinnvollere Methode suchen
-		var tournamentId = $('h1').data('id');
+	ended: function () {
+		// da dieses Template innerhalb von tournamentDetail > tournamentParticipants
+		var tournamentId = Template.parentData(2)._id;
 		var tournamentDate = Tournaments.findOne({_id: tournamentId}).date;
 		return tournamentDate < new Date();
+	},
+	mainbotName: function () {
+		return Robots.findOne(this.mainbot).name;
 	}
 });
 
-Template.tournamentRobot.events({
-	'click [name=signOutRobot]': function (event) {
-		// TODO: hier eine bessere Methode suchen um an das richtige Turnier zu kommen
-		var tournamentId = $('h1').data('id');
-		// this._id referenziert den Roboter des Templates
-		Meteor.call('signOutRobot', tournamentId, this._id, function (error, result) {
+Template.tournamentGroup.events({
+	'click [name=signOutFromTournament]': function (event) {
+		// Wegen verschachtelung is das Turniertemplate 2 weiter oben
+		// sollte beachtet werden, wenn an dieser Seite rumgebastelt wird
+		var tournamentId = Template.parentData(2)._id;
+		// this._id referenziert die Gruppe des Templates
+		Meteor.call('signOutFromTournament', tournamentId, this._id, function (error, result) {
 			if(error){
 				console.log(error.reason);
 			}

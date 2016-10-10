7c6c4b8299ab3ea8a173579798e0ea8ca62a9a4d
Jan Holub <j_holub11@cs.uni-kl.de>
2016-10-10 04:11:08 +0200

    Vorläufige Linux Eclipse Anleitung erstellt

--- client/Instructions/Linux/linux.js
+++ client/Instructions/Linux/linux.js
@@ -13,3 +13,21 @@ Template.linuxInstall.onRendered(function (){
 	$('h2').fitText(2);
 
 });
+
+Template.linuxEclipse.onRendered(function (){
+
+	// FlowTypeJS
+	$('p').flowtype({
+		minimum: 500,
+		maximum: 1000,
+		maxFont: 25
+	});
+
+	// FitTextJS
+	$('h1').fitText(1.8);
+
+	$('h2').fitText(2);
+
+});
+
+

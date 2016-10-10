46622809126653ffdd8517c23343668b8d648dba
Jan Holub <j_holub11@cs.uni-kl.de>
2016-10-10 04:00:39 +0200

    Anleitung zum Einrichten von Eclipse unter Windows eingepflegt

---
+++ client/Instructions/Windows/windows.js
@@ -0,0 +1,33 @@
+Template.windowsInstall.onRendered(function (){
+
+	// FlowTypeJS
+	$('p').flowtype({
+		minimum: 500,
+		maximum: 1000,
+		maxFont: 25
+	});
+
+	// FitTextJS
+	$('h1').fitText(1.8);
+
+	$('h2').fitText(2);
+
+});
+
+Template.windowsEclipse.onRendered(function (){
+
+	// FlowTypeJS
+	$('p').flowtype({
+		minimum: 500,
+		maximum: 1000,
+		maxFont: 25
+	});
+
+	// FitTextJS
+	$('h1').fitText(1.8);
+
+	$('h2').fitText(2);
+
+});
+
+

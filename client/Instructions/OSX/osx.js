dfdc7f6595d4a7e6175ba35ff695557ac7b9305c
Jan Holub <j_holub11@cs.uni-kl.de>
2016-10-10 02:16:02 +0200

    Anleitung zum Einrichten von Eclipse unter OS X eingepflegt

--- client/Instructions/OSX/osx.js
+++ client/Instructions/OSX/osx.js
@@ -13,3 +13,21 @@ Template.osxInstall.onRendered(function (){
 	$('h2').fitText(2);
 
 });
+
+Template.osxEclipse.onRendered(function (){
+
+	// FlowTypeJS
+	$('p').flowtype({
+		minimum: 500,
+		maximum: 1000,
+		maxFont: 25
+	});
+
+	// FitTextJS
+	$('h1').fitText(1.8);
+
+	$('h2').fitText(2);
+
+});
+
+

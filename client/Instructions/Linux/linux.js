b90dede7eccc58b4b56ede24ce1da89d75686a54
Jan Holub <j_holub11@cs.uni-kl.de>
2016-10-10 02:34:32 +0200

    Anleitung zum installieren der Programme unter Linux eingepflegt

---
+++ client/Instructions/Linux/linux.js
@@ -0,0 +1,15 @@
+Template.linuxInstall.onRendered(function (){
+
+	// FlowTypeJS
+	$('p').flowtype({
+		minimum: 500,
+		maximum: 1000,
+		maxFont: 25
+	});
+
+	// FitTextJS
+	$('h1').fitText(1.8);
+
+	$('h2').fitText(2);
+
+});

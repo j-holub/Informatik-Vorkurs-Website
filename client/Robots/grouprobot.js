9468d6a3f555aa06e5e200f2668e35f1d4a43643
Jan Holub <j_holub11@cs.uni-kl.de>
2017-10-13 15:11:06 +0200

    Downloadstate is visible in the group view as well

--- client/Robots/grouprobot.js
+++ client/Robots/grouprobot.js
@@ -9,5 +9,22 @@ Template.groupRobot.helpers({
 	// wird innerhalb des groupDetail Templates aufgerufen und benötigt dessen parentData
 	userIsGroupMember: function () {
 		return $.inArray(Meteor.userId(), Template.parentData().members) != -1;
-	}
+	},
+	belongsToUserOrIsAdmin: function() {
+		return Meteor.userId() == this.belongsTo || Roles.userIsInRole(Meteor.user(), ["admin"]);
+	},
+});
+
+
+Template.groupRobot.events({
+	'change .toggleBox': function(event){
+		// this._id referenziert die Id des Roboters mit dem das Template gerendert wurde
+		let robotId = this._id;
+		// status in der Datenbank updaten
+		Meteor.call('changeDownloadState', robotId, event.target.checked, function (error, result) {
+			if(error){
+				console.log(error.reason)
+			}
+		});
+	},
 });
